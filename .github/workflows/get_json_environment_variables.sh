6b7e0775d8d0104afb17f8218200a639e7032a5f
Emma Manteater <marmadilemanteater@proton.me>
2026-09-17 17:11:06 -0400

    test

--- .github/workflows/get_json_environment_variables.sh
+++ .github/workflows/get_json_environment_variables.sh
@@ -4,7 +4,7 @@ function export() {
     echo "$1" >> $GITHUB_ENV
 }
 
-node ./.github/workflows/get_json_kv_pairs.mjs | while read line
+node ./.github/workflows/get_json_kv_pairs.mjs $1 | while read line
 do
     export $line
 done
--- .github/workflows/get_json_kv_pairs.mjs
+++ .github/workflows/get_json_kv_pairs.mjs
@@ -1,10 +1,10 @@
 
 import { readFile } from 'fs/promises'
 
-const jsonString = (await readFile('./project.json')).toString()
+const jsonString = (await readFile(process.argv[2])).toString()
 
 const json = JSON.parse(jsonString)
 
 for (let key in json) {
-    console.log(`${key.toUpperCase()}=${json[key]}`)
+  console.log(`${key.toUpperCase()}=${json[key]}`)
 }
